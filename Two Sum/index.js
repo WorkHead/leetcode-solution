f5eb3c089e942ac7a05075421d644feee1684308
tanjiasheng <tanjiasheng@lvmama.com>
2018-03-29 17:00:36 +0800

    Two Sum

---
+++ Two Sum/index.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    for(let i = 0; i < nums.length; i++) {
+        const j = nums.indexOf(target - nums[i])
+        if(~j && j !== i) {
+            return [i, j]
+        }
+    }
+}; 
